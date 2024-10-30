e2259c35d8c151d241a42d8fc226fdc48742d0e3
Adrian Emmott <adrian.emmott@gmail.com>
2024-10-30 12:56:20 +0000

    tidy

--- Ratbags.Emails.API/Models/AppSettings.cs
+++ Ratbags.Emails.API/Models/AppSettings.cs
@@ -1,24 +1,23 @@
 ﻿using Ratbags.Core.Settings;
 
-namespace Ratbags.Emails.API.Models
+namespace Ratbags.Emails.API.Models;
+
+public class AppSettings : AppSettingsBase
 {
-    public class AppSettings : AppSettingsBase
-    {
-        public Mail Mail { get; set; } = new Mail();
-    }
+    public Mail Mail { get; set; } = new Mail();
+}
 
-    public class Mail
-    {
-        public string NoReplyEmail { get; set; } = string.Empty;
-        public SMTPSettings SMTPSettings { get; set; } = new SMTPSettings();
-    }
+public class Mail
+{
+    public string NoReplyEmail { get; set; } = string.Empty;
+    public SMTPSettings SMTPSettings { get; set; } = new SMTPSettings();
+}
 
-    public class SMTPSettings
-    {
-        public string Host { get; set; } = string.Empty;
-        public string Port { get; set; } = string.Empty;
-        public bool EnableSSL { get; set; }
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
+public class SMTPSettings
+{
+    public string Host { get; set; } = string.Empty;
+    public string Port { get; set; } = string.Empty;
+    public bool EnableSSL { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
 }
